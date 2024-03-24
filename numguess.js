66604e8d4ce5811127571fba3fab9cc8a86867e7
zubairxshah <zubairshahmasoomi@gmail.com>
2024-03-24 06:51:10 +0500

    First commit

---
+++ numguess.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const randNumber = Math.floor(Math.random() * 3 + 1);
+const userInput = await inquirer.prompt({
+    name: "input",
+    type: "number",
+    message: "Guess any number from 1 to 3 to win the game..",
+});
+if (randNumber === userInput.input) {
+    console.log("Congratulation! You Win..");
+}
+else {
+    console.log(`Sorry! The correct number is ${randNumber}.`);
+}
+;
